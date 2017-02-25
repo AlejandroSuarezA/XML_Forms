f8e3c1ae230523c9c075282c160efa7260ab6d3a
Jandro <jandro.suarez.92@gmail.com>
2017-02-25 23:10:42 +0100

    Segundo test

--- WebContent/js/firstExam.js
+++ WebContent/js/firstExam.js
@@ -11,24 +11,20 @@ window.onload = function() {
 	xhttp.open("GET", "../official_docs/questions.xml", true);
 	xhttp.send();
 	
-	// Obtener las preguntas del XML
-	function gestionarXml(dadesXml){
-		 var xmlDoc = dadesXml.responseXML;
-		 
-		 var questionInput1 = xmlDoc.getElementsByTagName("title")[0].innerHTML;
-		 ponerDatosInputHtml(questionInput1 , 1);
-	}
 		 
 		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 function ponerDatosInputHtml(title , number){
-			 document.getElementById("input" + number).innerHTML = t;
-			}
-		
+}
+// Obtener las preguntas del XML
+function gestionarXml(dadesXml){
+	var xmlDoc = dadesXml.responseXML;
+	
+	var tituloInput = xmlDoc.getElementsByTagName("title")[0].innerHTML;
+	ponerDatosInputHtml(tituloInput);
 }
 
+
+
+
+function ponerDatosInputHtml(t){
+	document.getElementById("input1").innerHTML = t;
+}
